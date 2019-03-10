acf26c552def8df566e6e7b5eeee04d2dcbd5322
Bini <abigiyabiniyam@gmail.com>
2019-03-10 15:55:50 -0400

    made a a lots of changes

--- src/main/java/com/blogmastery/demo/Initializer.java
+++ src/main/java/com/blogmastery/demo/Initializer.java
@@ -37,12 +37,19 @@ public class Initializer implements CommandLineRunner {
 		
 		BlogTag blogTagOne = blogTagRepo.save(new BlogTag("github"));
 		BlogTag blogTagTwo = blogTagRepo.save(new BlogTag("problems"));
+		BlogTag blogTagThree = blogTagRepo.save(new BlogTag("Sport"));
+		BlogTag blogTagFour = blogTagRepo.save(new BlogTag("Technology"));
 		
+		Genre genreFour = genreRepo.save(new Genre("Technology"));
 		Genre genreOne = genreRepo.save(new Genre("romance"));
 		Genre genreTwo = genreRepo.save(new Genre("sexual situations"));
+		Genre genreThree = genreRepo.save(new Genre("Sport"));
+		
 		
 		postRepo.save(new Post("First Blog Post","I really wish Donny let us use Gatsby.Js", authorOne, genreOne, blogTagOne));
 		postRepo.save(new Post("Secong Blog Post about Dogs", "I like dogs", authorTwo, genreTwo, blogTagTwo));		
+		postRepo.save(new Post("IS THIS THE START OF A GREAT UNITED STRIKE ", "Both players were stars in Paris and there will hopefully be much more to come as the season builds towards its climax. And while Solskjaer feels he hasn't been in charge long enough to see much development in Rashford, he's in no doubt about the youngster's quality.", authorTwo, genreThree, blogTagThree));
+		postRepo.save(new Post("The US Air Force’s jet-powered robotic wingman is like something out of a video game ", "Science", authorTwo, genreFour, blogTagFour));
 		
 		
 		
