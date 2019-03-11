021903ec69de5bdf0781539c56592a5f223b2693
mateofrito <46681151+mateofrito@users.noreply.github.com>
2019-03-11 08:34:47 -0400

    fixing master branch

--- src/test/java/com/blogmastery/demo/DemoApplicationTests.java
+++ src/test/java/com/blogmastery/demo/DemoApplicationTests.java
@@ -14,8 +14,12 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import com.blogmastery.demo.models.Author;
+import com.blogmastery.demo.models.BlogTag;
+import com.blogmastery.demo.models.Genre;
 import com.blogmastery.demo.models.Post;
 import com.blogmastery.demo.repositories.AuthorRepository;
+import com.blogmastery.demo.repositories.BlogTagRepository;
+import com.blogmastery.demo.repositories.GenreRepository;
 import com.blogmastery.demo.repositories.PostRepository;
 
 
@@ -33,10 +37,18 @@ public class DemoApplicationTests {
 	@Resource
 	private AuthorRepository authorRepo;
 	
+	@Resource 
+	private GenreRepository genreRepo;
+	
+	@Resource
+	private BlogTagRepository tagRepo;
 	
 	@Test
     public void shouldSaveAndLoadPost() {
-        Post post = postRepo.save(new Post("new blog", "This is my blog post", "Fry", "08 Mar 2019", "Sports", "Random"));
+		Genre testGenre = genreRepo.save(new Genre("sports"));
+        BlogTag testTag = tagRepo.save(new BlogTag("test"));
+        Author testAuthor = authorRepo.save(new Author("Fry"));
+		Post post = postRepo.save(new Post("new blog", "This is my blog post", testAuthor, testGenre, testTag));
         Long postId = post.getId();
  
         entityManager.persist(post);
@@ -52,7 +64,10 @@ public class DemoApplicationTests {
 	
 	@Test
     public void shouldSaveAndLoadAuthor() {
-		Post post = postRepo.save(new Post("new blog", "This is my blog post", "Fry", "08 Mar 2019", "Sports", "Random"));
+		Author testAuthor = authorRepo.save(new Author("Fry"));
+		Genre testGenre = genreRepo.save(new Genre("sports"));
+		BlogTag testTag = tagRepo.save(new BlogTag("test"));
+		Post post = postRepo.save(new Post("new blog", "This is my blog post", testAuthor, testGenre, testTag));
         Author author = authorRepo.save(new Author("my name", post));
         Long postId = author.getId();
  
@@ -65,6 +80,35 @@ public class DemoApplicationTests {
  
         assertThat(author.getAuthor(), is("my name"));
     }
-
+	@Test
+    public void shouldSaveAndLoadTag() {
+		//Post post = postRepo.save(new Post("new blog", "This is my blog post", "Fry", "08 Mar 2019", "Sports", "Random"));
+        BlogTag tag = tagRepo.save(new BlogTag("tag"));
+        Long tagId = tag.getId();
+ 
+        entityManager.persist(tag);
+        entityManager.flush();
+        entityManager.clear();
+ 
+        Optional<BlogTag> tagToFind = tagRepo.findById(tagId);
+        tag = tagToFind.get();
+ 
+        assertThat(tag.getTagName(), is("tag"));
+    }
+	@Test 
+	public void shouldSaveAndLoadGenre() {
+		Genre genre = genreRepo.save(new Genre("romance"));
+		Long genreID = genre.getId();
+		
+		entityManager.persist(genre);
+        entityManager.flush();
+        entityManager.clear();
+		
+        Optional<Genre> genreToFind = genreRepo.findById(genreID);
+        genre = genreToFind.get();
+        
+        assertThat(genre.getGenre(), is ("romance"));
+		
+	}
 	
 }
